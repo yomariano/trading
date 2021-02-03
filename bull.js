d1618a8d20e5f3c084c50ec88e63694347154107
Matias dos Santos <matias@getcrowder.com>
2021-02-03 02:02:34 -0300

    Market buy order and OCO sale order

--- bull.js
+++ bull.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import api from 'binance';
 
 dotenv.config();
+let binanceApi = new api.BinanceRest({ key: process.env.BINANCE_KEY, secret: process.env.BINANCE_SECRET });
 
 const waitForOrder = async (order) => {
   let anOrder = order;
@@ -14,24 +15,77 @@ const waitForOrder = async (order) => {
   return anOrder;
 };
 
-let binanceApi = new api.BinanceRest({ key: process.env.BINANCE_KEY, secret: process.env.BINANCE_SECRET });
-const account = await binanceApi.account();
-const quantityUsdt = Math.floor(parseFloat(account.balances.find((asset) => asset.asset === 'USDT').free));
-console.log(`Balance USDT ${quantityUsdt}`);
-const symbol = 'BNBUSDT';
-const orderPrice = 51.15;
-//const buyOrder  = await binanceApi.newOrder({symbol, type: 'MARKET', side: 'BUY', quoteOrderQty: quantityUsdt })
-const buyOrder = await binanceApi.newOrder({
-  symbol,
-  type: 'LIMIT',
-  side: 'BUY',
-  timeInForce: 'GTC',
-  quantity: Math.floor((quantityUsdt * 1000) / orderPrice) / 1000,
-  price: orderPrice,
-});
-console.log(buyOrder);
-console.log(await waitForOrder(buyOrder));
+const weightedAverage = (fills) => {
+  const wa = fills.reduce(
+    (counter, fill) => {
+      return {
+        totalQty: counter.totalQty + parseFloat(fill.qty),
+        avgPrice: counter.avgPrice + parseFloat(fill.qty) * parseFloat(fill.price),
+      };
+    },
+    { totalQty: 0, avgPrice: 0 }
+  );
+  return wa.avgPrice / wa.totalQty;
+};
 
-// const sellOrder  = await binanceApi.newOcoOrder({symbol, type: 'MARKET', side: 'SELL', quoteOrderQty: quantityUsdt })
+const safePrice = (price, precision) => {
+  const m = Math.pow(10, precision);
+  return Math.floor(price * m) / m;
+};
 
-// console.log(sellOrder)
+const getFilledPrice = (order) => {
+  switch (order.type) {
+    case 'LIMIT':
+      return parseFloat(order.price);
+    case 'MARKET':
+      return parseFloat(order.cummulativeQuoteQty) / parseFloat(order.executedQty); // weightedAverage(order.fills);
+    default:
+      return null;
+  }
+};
+
+(async () => {
+  const account = await binanceApi.account();
+  const quantityUsdt = safePrice(parseFloat(account.balances.find((asset) => asset.asset === 'USDT').free), 3);
+  console.log(`Balance USDT ${quantityUsdt}`);
+  const symbol = 'XRPUSDT';
+  //   const orderPrice = 50.75;
+  const buyOrder = await binanceApi.newOrder({ symbol, type: 'MARKET', side: 'BUY', quoteOrderQty: quantityUsdt });
+
+  //   LIMIT Order
+  //   ---------
+  //   const buyOrder = await binanceApi.newOrder({
+  //     symbol,
+  //     type: 'LIMIT',
+  //     side: 'BUY',
+  //     timeInForce: 'GTC',
+  //     quantity: Math.floor((quantityUsdt * 1000) / orderPrice) / 1000,
+  //     price: orderPrice,
+  //   });
+
+  const filledOrder = await waitForOrder(buyOrder);
+  const buyPrice = getFilledPrice(filledOrder);
+
+  const price = safePrice(buyPrice * 1.01, 6);
+  const stopPrice = safePrice(buyPrice * 0.99, 6);
+  const stopLimitPrice = safePrice(buyPrice * 0.985, 6);
+
+  const sellQuery = {
+    symbol,
+    price,
+    stopPrice,
+    stopLimitPrice,
+    stopLimitTimeInForce: 'GTC',
+    side: 'SELL',
+    quantity: parseFloat(filledOrder.executedQty),
+    timestamp: new Date().getTime(),
+  };
+  console.log(sellQuery);
+
+  try {
+    const sellOrder = await binanceApi.newOcoOrder(sellQuery);
+    console.log(sellOrder);
+  } catch (e) {
+    console.log(e);
+  }
+})();
